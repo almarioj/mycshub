b63b5e510a6c4244689f05e95542d734c2b1dd18
June Andrea Celo <junec@junec.local>
2016-10-06 20:31:10 +0800

    Initial theme setup
    ADD base templates
    ADD assests for landing page and dashboard theme
    ADD landing page template

--- conf/urls.py
+++ conf/urls.py
@@ -20,6 +20,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^', TemplateView.as_view(template_name="index.html"))
 ]
 
 if settings.DEBUG:
